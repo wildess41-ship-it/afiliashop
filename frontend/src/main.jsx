d9bb24c59d83ad2194771f29ae219019c0227ac3
Wildes <wildess41@gmail.com>
2026-04-26 10:26:59 -0300

    conectando frontend ao backend online

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,6 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
+import axios from "axios";
+
+axios.defaults.baseURL = "https://afiliashop-backend.onrender.com";
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
